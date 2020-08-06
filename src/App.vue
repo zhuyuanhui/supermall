5ad71f500096f8f5ae4c2392c76f67dfa489d4fe
zyuhui <yuanhui342@live.com>
2020-08-06 12:51:20 +0800

    目录结构

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-  </div>
+  <div id="app"></div>
 </template>
 
 <script>
@@ -18,8 +16,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
